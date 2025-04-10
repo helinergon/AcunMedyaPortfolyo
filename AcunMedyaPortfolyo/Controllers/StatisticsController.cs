49e6afef5a5d3c93302af3e836ded35fa3900b7e
helinergon <helinergon82@gmail.com>
2025-04-10 21:47:11 +0300

    - İstatistikler Yapıldı!

--- AcunMedyaPortfolyo/Controllers/StatisticsController.cs
+++ AcunMedyaPortfolyo/Controllers/StatisticsController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AcunMedyaPortfolyo.Models;
 
 namespace AcunMedyaPortfolyo.Controllers
 {
     public class StatisticsController : Controller
     {
-        // GET: Statistics
+        DbAcunMedyaProject1Entities1 db = new DbAcunMedyaProject1Entities1 ();
         public ActionResult Index()
         {
+            ViewBag.CategoryCount = db.Tbl_Category.Count();
+            ViewBag.ProjectCount = db.Tbl_Project.Count();
+            ViewBag.ContactCount = db.Tbl_Contact.Count();
+            ViewBag.TestimonialCount = db.Tbl_Testimonial.Count();
             return View();
         }
     }
